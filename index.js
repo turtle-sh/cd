a6423b0efbd90cc5bae1e2bd6bc46fbce371830b
itsjoesullivan <itsjoesullivan@gmail.com>
2013-10-02 21:21:07 -0400

    init

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+var path = require('path');
 define({
   "expr": /^cd/,
   "fn": function(arg) {
+    console.log(arg[1]);
     var newPath = path.resolve(this.cwd(), arg[1] );
     this.chdir(newPath);
     this.exit();
